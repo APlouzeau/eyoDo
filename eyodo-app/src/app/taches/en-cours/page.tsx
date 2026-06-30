4c69fc1cb41568c0e23daeb46a868911dfc5923f
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2026-06-30 18:00:09 +0200

    front wip

--- eyodo-app/src/app/taches/en-cours/page.tsx
+++ eyodo-app/src/app/taches/en-cours/page.tsx
@@ -5,6 +5,11 @@ export default function EnCours() {
         <div className="flex min-h-screen w-full flex-col items-center">
             <h2 className="text-2xl font-bold mb-4 text-center pt-8">En cours</h2>
             <div className="w-full gap-4 p-8">
+                <div className="flex justify-end">
+                    <button className="flex bg-gray-800 text-white px-4 py-2 rounded mb-4 font-semibold">
+                        Ajouter une tâche
+                    </button>
+                </div>
                 <TaskTable />
             </div>
         </div>
